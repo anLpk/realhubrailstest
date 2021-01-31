94d2cd06352b31217515efa6b213c8e0e6906364
anil pak <anilpak35@gmail.com>
2021-02-01 01:06:25 +1100

    api call for get_orders method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,16 @@
+require 'json'
 class OrdersController < ApplicationController
+    URL = "https://app.realhublive.com/api/v2/"
+    HEADERS = {"x-api-token".to_sym => ENV["API_KEY"]}  
     def show
+        @orders = get_orders[0..10]
+        #Getting all orders takes too much time. I took just first 10 of them
         
+    end
+
+    def get_orders
+      pak = JSON.parse(RestClient.get(URL + "orders.json?include_order_agency=true&include_order_campaign=true&include_order_items=true", HEADERS))
+    #   raise
     end
     
 end
